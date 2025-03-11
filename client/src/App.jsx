f10c4e76169187f81cf877c214b76aae798a2abc
Akilesh <akileshakileshs1234@gmail.com>
2025-03-11 23:51:12 +0530

    Created the initial project structure

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css"; // Optional: Custom styles
+
+function App() {
+  return (
+    <div className="app-container text-5xl">
+
+    </div>
+  );
+}
+
+export default App;
